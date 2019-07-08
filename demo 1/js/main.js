2af78f581b9dc9ebee9d5f29acb5436f374c4048
Jpegzilla <jpegzilla@gmail.com>
2019-07-08 04:24:08 -0400

    initial

---
+++ demo 1/js/main.js
@@ -0,0 +1,13 @@
+// contains all javascript for basic operation. essentially everything not covered in one of the other scripts.
+
+window.onload = () => {
+  console.log("loaded 👍");
+  // intro transitions
+  // setTimeout(() => {
+  //   document.getElementById("preloader").classList.add("hidden");
+  //   setTimeout(
+  //     () => document.getElementsByTagName("main").classList.remove("hidden"),
+  //     500
+  //   );
+  // }, 1500);
+};
---
+++ demo 2/js/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  console.log("loaded 👍");
+  // intro transitions
+  // setTimeout(() => {
+  //   document.getElementById("preloader").classList.add("hidden");
+  //   setTimeout(
+  //     () => document.getElementsByTagName("main").classList.remove("hidden"),
+  //     500
+  //   );
+  // }, 1500);
+};
